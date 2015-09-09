c5d829c852fe9c7d803775ac66c60066015d4dd9
Betsy Lorton <elorton@gmail.com>
2015-09-08 21:44:33 -0500

    Test existing Todo model method

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   subject { Todo.new(title: 'stuff to do') }
-  describe 'completed' do
-    it 'returns true for completed todos' do
-      expect(subject.completed).to be_false
+
+  describe 'title=' do
+    it 'strips whitespace' do
+      expect do
+        subject.title = '   different title      '
+      end.to change {subject.title}.to('different title')
     end
+
+    it 'saves new title' do
+      expect do
+        subject.title = 'new title'
+      end.to change {subject.title}.to('new title')
+    end
+
   end
 end
