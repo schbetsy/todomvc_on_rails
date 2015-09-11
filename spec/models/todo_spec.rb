94a1f08293d319f58e6c2a63521191838cdd19ae
Betsy Lorton <elorton@gmail.com>
2015-09-11 16:22:47 -0500

    Test behavior of Todo#completed?

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,6 +5,34 @@ RSpec.describe Todo, type: :model do
 
   it { should have_many :subtasks }
 
+  describe 'completed?' do
+    subject { Todo.create(title: 'stuff to do') }
+
+    context 'has subtasks' do
+      let(:st1) { subject.subtasks.create(title: 'one') }
+
+      it 'returns true if all subtasks are completed' do
+        st1.update_attributes(completed: true)
+        expect(subject.completed?).to eq true
+      end
+      it 'returns false if any subtask is not completed' do
+        st1.update_attributes(completed: false)
+        expect(subject.completed?).to eq false
+      end
+    end
+
+    context 'has no subtasks' do
+      it 'returns true if `completed` is true in database' do
+        subject.update_attributes(completed: true)
+        expect(subject.completed?).to eq true
+      end
+      it 'returns false if `completed` is false in database' do
+        subject.update_attributes(completed: false)
+        expect(subject.completed?).to eq false
+      end
+    end
+  end
+
   describe 'title=' do
     it 'strips whitespace' do
       expect do
